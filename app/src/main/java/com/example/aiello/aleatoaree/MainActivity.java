a8ede8919abd95db41266e28b351c5ba37c4c14d
Anthony <aiello83@hotmail.com>
2016-04-14 15:40:41 +0200

    Changement action

--- app/src/main/java/com/example/aiello/aleatoaree/MainActivity.java
+++ app/src/main/java/com/example/aiello/aleatoaree/MainActivity.java
@@ -1,15 +1,18 @@
 package com.example.aiello.aleatoaree;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.content.Intent;
-import android.app.Activity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 // Le debut de l'application
 public class MainActivity extends AppCompatActivity {
@@ -21,6 +24,34 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //Layout principal
+        LinearLayout contentMain = (LinearLayout)findViewById(R.id.linearLayout1);
+
+        ArrayList<EditText> InputListe = new ArrayList<EditText>();
+
+        EditText myEdit1 = new EditText(this);
+        myEdit1.setWidth(200);
+        myEdit1.setHeight(200);
+        myEdit1.setHint("#1");
+
+        EditText myEdit2 = new EditText(this);
+        myEdit2.setWidth(500);
+        myEdit2.setHeight(200);
+        myEdit2.setHint("#2");
+
+        //on ajoute les 2 inputs de bases obligatoires
+        InputListe.add(myEdit1);
+        InputListe.add(myEdit2);
+
+        for (EditText item : InputListe) { //pour chaque item de la liste d'input on l'ajoute à la vue
+            contentMain.addView(item);
+        }
+
+
+
+
+
+        /*** Gestion du bouton flotant***/
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,7 +80,9 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
-            return true;
+            //return true;
+            Snackbar.make(this.findViewById(android.R.id.content), "Hello world", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
         }
 
         return super.onOptionsItemSelected(item);
